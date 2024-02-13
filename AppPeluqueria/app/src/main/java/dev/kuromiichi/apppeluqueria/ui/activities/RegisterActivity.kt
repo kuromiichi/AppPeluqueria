dfbbf581f1011496a44f11aa90167a9c5e43937a
sergiomisas <misas_virseda@hotmail.es>
2024-02-13 19:39:49 +0100

    Register user in firestore database

--- AppPeluqueria/app/src/main/java/dev/kuromiichi/apppeluqueria/ui/activities/RegisterActivity.kt
+++ AppPeluqueria/app/src/main/java/dev/kuromiichi/apppeluqueria/ui/activities/RegisterActivity.kt
@@ -6,13 +6,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dev.kuromiichi.apppeluqueria.R
 import dev.kuromiichi.apppeluqueria.databinding.ActivityRegisterBinding
+import dev.kuromiichi.apppeluqueria.models.User
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private val auth: FirebaseAuth by lazy { Firebase.auth }
+    private val db by lazy { Firebase.firestore }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,9 +63,23 @@ class RegisterActivity : AppCompatActivity() {
                     ).show()
                     return@setOnClickListener
                 }
+                phone.length != 9 -> {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.toast_invalid_phone),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                !email.matches("^[\\w-.]+@([\\w-]+.)+[\\w-]{2,4}\$".toRegex()) -> {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.toast_invalid_email),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
                 else -> auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        // TODO: add to database
+                        createUser()
                         startActivity(Intent(this, MainActivity::class.java))
                     } else Toast.makeText(
                         this,
@@ -76,5 +93,18 @@ class RegisterActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+    }
+
+    private fun createUser() {
+        db.collection("users")
+            .document(auth.currentUser!!.uid)
+            .set(
+                User(
+                    name = binding.tilName.editText?.text.toString(),
+                    email = binding.tilEmail.editText?.text.toString(),
+                    phone = binding.tilPhone.editText?.text.toString(),
+                    uuid = auth.currentUser!!.uid,
+                )
+            )
     }
 }
