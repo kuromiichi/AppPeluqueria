8e2c211fbf0d2c18552655917ac9733fd65631f4
kuromiichi <emmalikescoding@gmail.com>
2024-02-14 20:03:35 +0100

    updated safeargs and google services dependencies

--- AppPeluqueria/build.gradle.kts
+++ AppPeluqueria/build.gradle.kts
@@ -3,9 +3,9 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        val nav_version = "2.5.3"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("com.google.gms:google-services:4.4.1")
+        val safeArgsVersion = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$safeArgsVersion")
 
     }
 }
