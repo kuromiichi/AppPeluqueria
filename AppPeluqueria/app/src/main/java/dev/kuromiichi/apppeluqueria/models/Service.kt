9acd9f87849e91f3076280e2a685ecb808b23fd7
kuromiichi <emmalikescoding@gmail.com>
2024-02-14 20:52:48 +0100

    modified models for firebase compatibility

--- AppPeluqueria/app/src/main/java/dev/kuromiichi/apppeluqueria/models/Service.kt
+++ AppPeluqueria/app/src/main/java/dev/kuromiichi/apppeluqueria/models/Service.kt
@@ -1,6 +1,10 @@
 package dev.kuromiichi.apppeluqueria.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Service(
-    val name: String,
-    val time: String
-)
+    val name: String = "",
+    val time: String = ""
+) : Parcelable
